223d0b2c97c23219c70dd8f682c7c0a6ea6db654
Michael Newman <coloradohokie@gmail.com>
2019-12-16 16:51:21 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,10 +11,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movie_titles = []
+#  movie_titles = []
   director = 0
   x = 0
-  while x < nds[director][:movies].length do
+  while x < directors_database[director][:movies].length do
 #    movie_titles[x] = nds[director][:movies][x][:title]
     p nds[director][:movies][x][:title]
     x += 1
